8af0ce9c160387fb47aa4711681928e4b781d9ec
neo <abdulzoha786@gmail.com>
2022-07-15 09:24:31 +0300

    updated: 'python' dir

--- python/exercise_files/projects/AI_projects/Alexa(Hindi)/wb.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-from speak import speak
-
-def wb(query):
-    speak("OK sir, this is that I found for your Search")
-    query=query.replace("alexa", "")
-    query=query.replace("youtube search")
--- python/myPython/multi_threading/01_current_thread.py
+++
@@ -1,4 +0,0 @@
-from threading import Thread, current_thread
-
-main = current_thread()
-print(main.name) #to get Current Thread Name
