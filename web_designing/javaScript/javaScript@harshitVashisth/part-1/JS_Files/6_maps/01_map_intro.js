036c766cf63ba5151aa9fa0af044f486b38a33d5
Abdul Zoha <abdulzoha786@gmail.com>
2023-02-25 22:07:11 +0300

    On branch master Changes to be committed: 	modified:   web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/01_map_intro.js 	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/02_keys_method.js 	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/03_array_as_key.js 	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/04_map_with_value.js 	modified:   web_designing/javaScript/javaScript@harshitVashisth/part-1/timeStamp.txt

--- web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/01_map_intro.js
+++ web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/01_map_intro.js
@@ -12,18 +12,24 @@ object can only have string or symbol as key
 MAP:-
 in map you can use anything as key (like array, number, string)
 */
+
 /* object literal */
 const person = {
     name: "Harshit",
     age: 27
 }
 
-/* map */
-const person2 = new Map();
-console.log(person2);
+/* defining map */
+const person2 = new Map(); //empty map
+// console.log(person2);
 
 /* adding items */
 person2.set("Name", "Harshit");
+person2.set("age", 27);
+person2.set(1, "one");
 console.log(person2);
 
-/* 8:16:00 / 11:08:32 */
+/* accessing items */
+let nm = person2.get("Name");
+console.log(nm);
+console.log(person2.get(1));
---
+++ web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/6_maps/04_map_with_value.js
@@ -0,0 +1,7 @@
+const person = new Map([["name", "Harshit"], ["age", 28], ["class", "12th"]]);
+console.log(person);
+
+
+
+
+/* 8:23:00 / 11:08:32 */
