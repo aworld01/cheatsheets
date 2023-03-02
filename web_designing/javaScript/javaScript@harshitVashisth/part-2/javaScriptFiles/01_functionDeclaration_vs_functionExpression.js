6b86ff710e27d60e5f357edb93fbbe3eb46e978a
Abdul Zoha <abdulzoha786@gmail.com>
2023-03-02 21:01:23 +0300

    On branch master Changes to be committed: 	renamed:    web_designing/javaScript/javaScript@harshitVashisth/part-1/index.html -> web_designing/javaScript/javaScript@harshitVashisth/index.html 	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-2/javaScriptFiles/00_intro.js 	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-2/javaScriptFiles/01_functionDeclaration_vs_functionExpression.js 	new file:   web_designing/javaScript/javaScript@harshitVashisth/timeStamp2.txt 	new file:   web_designing/javaScript/javaScript@harshitVashisth/timeStamp3.txt 	renamed:    web_designing/javaScript/javaScript@harshitVashisth/part-1/timeStamp__finished.txt -> web_designing/javaScript/javaScript@harshitVashisth/timeStamp3__finished.txt

---
+++ web_designing/javaScript/javaScript@harshitVashisth/part-2/javaScriptFiles/01_functionDeclaration_vs_functionExpression.js
@@ -0,0 +1,13 @@
+/* function declaration */
+function myFunc(){
+    console.log("Hello world...");
+};
+
+
+/* function expression */
+function myFunc2(){
+    console.log("Hello world...");
+};
+
+myFunc();
+myFunc2();
