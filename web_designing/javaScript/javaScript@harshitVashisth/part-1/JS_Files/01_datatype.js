ebcd9fd54d1b23e1af110c6f69a381dd6bfd5536
Abdul Zoha <abdulzoha786@gmail.com>
2023-03-02 16:27:03 +0300

    On branch master Changes to be committed: 	renamed:    JS_Files/01_hello_world.js -> JS_Files/00_hello_world.js 	new file:   JS_Files/01_datatype.js 	deleted:    datatypes.txt

---
+++ web_designing/javaScript/javaScript@harshitVashisth/part-1/JS_Files/01_datatype.js
@@ -0,0 +1,18 @@
+/* primitive data types */
+
+/*
+number 2,4,5,6
+BigIng
+Symbol
+null
+*/
+
+let str = "Harshit"; //string
+let yes = true; //boolean
+let no = false; //boolean
+let unknown; //undifined
+
+console.log(str, typeof str);
+console.log(yes, typeof yes);
+console.log(no, typeof no);
+console.log(unknown, typeof unknown);
