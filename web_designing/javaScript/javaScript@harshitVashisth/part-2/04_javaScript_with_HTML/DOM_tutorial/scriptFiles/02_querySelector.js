010191d52ff8d65034d351b97fa455c66432cbf7
Abdul Zoha <abdulzoha786@gmail.com>
2023-03-06 22:07:02 +0300

    On branch master Changes to be committed:
    	renamed:    web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/00_intro.js -> web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/00_intro.js
    	modified:   web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/02_querySelector.js
    	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/04_textContent_vs_innerText.js
    	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/05_change_the_styles_of_elements.js
    	new file:   web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/06_get_and_set_attributes.js
    	modified:   web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/test.html
    	modified:   web_designing/javaScript/javaScript@harshitVashisth/timeStamp2.txt

--- web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/02_querySelector.js
+++ web_designing/javaScript/javaScript@harshitVashisth/part-2/04_javaScript_with_HTML/DOM_tutorial/scriptFiles/02_querySelector.js
@@ -4,6 +4,6 @@
 
 // const heading = document.querySelector(".p1"); //this will return only first element
 
-const heading = document.querySelectorAll(".p1"); //this will return nodeList
+const heading = document.querySelectorAll(".p1"); //it returns a nodeList
 
 console.log(heading);
