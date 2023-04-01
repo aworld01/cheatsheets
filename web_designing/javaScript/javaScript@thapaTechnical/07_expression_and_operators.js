84658c29ca4818e830700aebfd522255c87a9a18
Abdul Zoha <abdulzoha786@gmail.com>
2023-04-01 22:03:44 +0300

    On branch master Changes to be committed: 	new file:   web_designing/javaScript/javaScript@thapaTechnical/06_NaN.js 	new file:   web_designing/javaScript/javaScript@thapaTechnical/07_expression_and_operators.js 	new file:   web_designing/javaScript/javaScript@thapaTechnical/08_arithmetic_oprators.js 	new file:   web_designing/javaScript/javaScript@thapaTechnical/09_increment_and_decrement_operator.js 	modified:   web_designing/javaScript/javaScript@thapaTechnical/timesteps.txt

---
+++ web_designing/javaScript/javaScript@thapaTechnical/07_expression_and_operators.js
@@ -0,0 +1,25 @@
+/*
+The simple assignment operator is equal (=)
+An assignment operator assigns a value to its left  operand
+example:
+    var x = 15;
+
+
+console.log(5+20);
+5, 20: is operand
++: is operator
+operand + operator = expression
+*/
+
+/* example1 */
+// console.log(5+20);
+
+
+/* example2 */
+var x = 5;
+var y = 5;
+console.log(5+5);
+
+console.log("is both the x and y are equal or not?", (x == y));
+
+console.log(`Is both the x and y are equal or not? ${x==y}`); //es6
