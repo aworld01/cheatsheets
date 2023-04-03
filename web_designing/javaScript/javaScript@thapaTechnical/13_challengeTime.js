32181cd49f1cac6e071c1c4dac45bf7b5215af62
Abdul Zoha <abdulzoha786@gmail.com>
2023-04-03 16:38:25 +0300

    On branch master Changes to be committed: 	modified:   web_designing/javaScript/javaScript@thapaTechnical/12_string_operators.js 	new file:   web_designing/javaScript/javaScript@thapaTechnical/13_challengeTime.js 	modified:   web_designing/javaScript/javaScript@thapaTechnical/timesteps.txt

---
+++ web_designing/javaScript/javaScript@thapaTechnical/13_challengeTime.js
@@ -0,0 +1,29 @@
+/* What will be the output of 3**3 */
+// console.log(3**3); //27 (power)
+
+/* What will be the output, when we add a number and a string */
+// console.log(7+" world");
+
+
+
+/* write a program to swap two numbers */
+/* example1 */
+// var a = 5;
+// var b = 10;
+
+// var c = b;
+// b = a;
+// a = c;
+// console.log("The value of a is: "+a);
+// console.log("The value of b is: "+b);
+
+
+/* write a program to swap two numbers without using third variable */
+var a = 5;
+var b = 10;
+
+a = a+b; // a = 15
+b = a-b; // b = 5
+a = a-b; // a = 10
+console.log("The value of a is: "+a);
+console.log("The value of b is: "+b);
