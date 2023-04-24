a3de3b8263cde738800791506fdec14fb87709d8
Abdul Zoha <abdulzoha786@gmail.com>
2023-04-24 19:37:34 +0300

    On branch master Changes to be committed: 	modified:   Video editing/Blender/Bevel_tool.txt 	modified:   python/tutorials/complete_python_basics_for_automation/modules/09_suprocess_module.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/10_getpass_module.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/11_sys_module.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/myfile.txt

--- python/tutorials/complete_python_basics_for_automation/modules/09_suprocess_module.py
+++ python/tutorials/complete_python_basics_for_automation/modules/09_suprocess_module.py
@@ -1,7 +1,36 @@
 """
 subprocess module is used to execute Operating System commands
+==============================================================
+
+subprocess.run('ls') #to run a simple command
+
+subprocess.run('ls -la', shell=True) #to run command with arguments
+
+cmd = "ls -lrt".split() #to convert into list (["ls","-lrt"])
+
+print(p1.stdout.decode()) #stdout.decode() converts binary into text
+
+universal_newlines=True  #gives output as string (default output comes in byte code)
+
+stdout=subprocess.PIPE #returns output
+
+stderr=subprocess.PIPE #returns error
+
+out = out.splitlines() #to convert output into list.
+====================================================
+
+
 comm = "ls -la"
 comm2 = "ls -lrt"
+
+shell = True (takes command in string and run in new shell)
+example:
+    cmd = "dir" (windows)
+    cmd = "ls -lrt" (Linux)
+
+shell = False (takes command in list and run in same shell. It does'n work on your os environment variables)
+example:
+    cmd = ["ls","-lrt"]
 
 Note: stdout=subprocess.PIPE == capture_output=True
 examplt:
@@ -19,75 +48,22 @@ syntax:
     print()
     print(f"The error is: \n\t{err}")
 
-example2:
-    comm = "ls -la"
-    p1 = subprocess.run(comm, shell=True)
-    print(p1)
-    print(p1.args)
-    print(p1.returncode)
-    print(p1.stdout)
-
-example3:
+example1:
     with open('outPut.txt', 'w') as wf:
     p1 = subprocess.run("ls -l", shell=True, stdout=wf, text=True) #to save output into textFile.
 
-exmple4:
+exmple2:
     file = subprocess.Popen(["mousepad", "myFile"]) #to open file with file name
     usr_inp = input("Type exit to exit: ")
     if usr_inp == "exit":
         file.terminate()
 exmaple5:
     browser = subprocess.Popen(["/bin/firefox", "www.facebook.com"])
-=========================================================================
-
-subprocess.run('ls') #to run a simple command
-
-subprocess.run('ls -la', shell=True) #to run command with arguments
-
-cmd = "ls -lrt".split() #to convert into list (["ls","-lrt"])
-
-print(p1.stdout.decode()) #stdout.decode() converts binary into text
-
-universal_newlines=True  #gives output as string (default output comes in byte code)
-
-stdout=subprocess.PIPE #returns output
-
-stderr=subprocess.PIPE #returns error
-
-out = out.splitlines() #to convert output into list.
-
-shell = True (takes command in string and run in new shell)
-example:
-    cmd = "dir" (windows)
-    cmd = "ls -lrt" (Linux)
-
-shell = False (takes command in list and run in same shell)
-example:
-    cmd = ["ls","-lrt"]
-
-    
-15:00 / 28:16
 """
 import subprocess as sp
 
-
-    # print(p1)
-    # print(p1.args)
-    # print(p1.returncode)
-    # print(p1.stdout)
-
 """example1 (The output will be in byte code)"""
-cmd = "dir"
-com = sp.Popen(cmd, shell=True)
-print(com)
-com.wait() #importent
-
-
-
-
-
-"""example1 (The output will be in byte code)"""
-# cmd = "dir"
+# cmd = "notepad"
 # com = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
 # rc = com.wait()
 # out, err = com.communicate()
@@ -106,7 +82,7 @@ com.wait() #importent
 
 
 """example2 (The output will be in string)"""
-# cmd = "dir"
+# cmd = "notepad"
 # com = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True) #The output will be in string
 # rc = com.wait()
 # out, err = com.communicate()
@@ -125,7 +101,7 @@ com.wait() #importent
 
 
 """example3 (The output will be converted into list)"""
-# cmd = "dir"
+# cmd = "notepad"
 # com = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True) #The output will be in string
 # rc = com.wait()
 # out, err = com.communicate()
@@ -142,3 +118,39 @@ com.wait() #importent
 
 # print(f"The error is: \n{err}")
 # print(type(err))
+
+
+
+"""example4 (open Notepad with 'myfile.txt' file name)"""
+# cmd = "notepad myfile.txt"
+# com = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+# rc = com.wait()
+# out, err = com.communicate()
+# out = out.splitlines() #to convert output into list.
+# err = err.splitlines()
+
+# print(f"The return code is: {rc}")
+# print(type(rc))
+# print()
+
+# print(f"The output is: \n{out}")
+# print(type(out))
+# print()
+
+# print(f"The error is: \n{err}")
+# print(type(err))
+
+
+
+"""example5 (final code)"""
+# cmd = "notepad myfile.txt"
+# com = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+# rc = com.wait()
+
+
+"example6 "
+cmd = "notepad myfile.txt"
+file = sp.Popen(cmd)
+usr_inp = input("Type exit to exit: ")
+if usr_inp == "exit":
+    file.terminate()
---
+++ python/tutorials/complete_python_basics_for_automation/modules/11_sys_module.py
@@ -0,0 +1,22 @@
+"""
+The sys() module is used to work with python runtime environment.
+The sys() module provides functions and variables used to manipulate different parts of the Python runtime environment.
+"""
+
+import sys
+
+# print(dir(sys)) #to list functions and variables
+# help(sys) #to read documentation
+
+"""examples"""
+# print(sys.platform) #to see installed python bit
+# print()
+# print(sys.version) #to see python version
+# print()
+# print(sys.path) #to check where python go and check for it requirments
+
+
+
+"""more other examples"""
+sys.exit() #to stop python runtime environment
+print("Hello world!!!")
