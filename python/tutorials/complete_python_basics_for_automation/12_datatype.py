f93aebcd38aa47f0b2b4f3d5b6f2545b58725670
Abdul Zoha <abdulzoha786@gmail.com>
2023-04-28 20:48:44 +0300

    On branch master Changes to be committed: 	renamed:    python/AI/deepLearning/01_firstProgram.ipynb -> python/AI/02_DL/01_firstProgram.ipynb 	renamed:    python/AI/deepLearning/NLP/01_tokaenization.ipynb -> python/AI/02_DL/NLP/01_tokaenization.ipynb 	renamed:    python/AI/deepLearning/NLP/02_padding/01_padding_save_drop_words.ipynb -> python/AI/02_DL/NLP/02_padding/01_padding_save_drop_words.ipynb 	renamed:    python/AI/deepLearning/NLP/02_padding/02_padding_pass_data_in_Nural_Network.ipynb -> python/AI/02_DL/NLP/02_padding/02_padding_pass_data_in_Nural_Network.ipynb 	modified:   python/tutorials/complete_python_basics_for_automation/12_datatype.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/02_import_module.py -> python/tutorials/complete_python_basics_for_automation/modules/01_import_module.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/03_platformModule.py -> python/tutorials/complete_python_basics_for_automation/modules/02_platformModule.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/09_suprocess_module.py -> python/tutorials/complete_python_basics_for_automation/modules/03_suprocess_module.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/10_getpass_module.py -> python/tutorials/complete_python_basics_for_automation/modules/04_getpass_module.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/05_json_module/01_Json_load.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/05_json_module/02_Json_dump.py 	new file:   python/tutorials/complete_python_basics_for_automation/modules/05_json_module/testDump.json 	new file:   python/tutorials/complete_python_basics_for_automation/modules/05_json_module/testLoad.json 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/11_sys_module/01_sys_module.py -> python/tutorials/complete_python_basics_for_automation/modules/06_sys_module/01_sys_module.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/11_sys_module/02_sys.argv.py -> python/tutorials/complete_python_basics_for_automation/modules/06_sys_module/02_sys.argv.py 	renamed:    python/tutorials/complete_python_basics_for_automation/modules/11_sys_module/03_exercise.py -> python/tutorials/complete_python_basics_for_automation/modules/06_sys_module/03_exercise.py

---
+++ python/tutorials/complete_python_basics_for_automation/12_datatype.py
@@ -0,0 +1,20 @@
+"""
+Every value in python has a datatype.
+Since everything is an object in Python programming, data types are actually classes and variables are instance (object) of these classes.
+
+13:00 / 22:33
+"""
+string = "Hello world" #string
+"""boolean"""
+boolean1 = True
+boolean2 = False
+"""number"""
+num = 123
+flt = 2.5
+com = 3+4j
+
+
+"""printing infomations"""
+print(f"Data => {string}")
+print(f"Type => {type(string)}") #to check datatype
+print(f"Memory location => {id(string)}") #to check memory location
