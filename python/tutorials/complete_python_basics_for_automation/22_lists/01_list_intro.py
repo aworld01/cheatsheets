2f259c63f31e7b5f3b7d6d3ed3e01495132403ed
Abdul Zoha <abdulzoha786@gmail.com>
2023-05-13 19:06:27 +0300

    On branch master Changes to be committed: 	modified:   python/tutorials/complete_python_basics_for_automation/22_lists/01_list_intro.py

--- python/tutorials/complete_python_basics_for_automation/22_lists/01_list_intro.py
+++ python/tutorials/complete_python_basics_for_automation/22_lists/01_list_intro.py
@@ -2,12 +2,30 @@
 print(bool(empty_list)) #False
 print(bool(none-empty_list)) #True
 
-06:00 / 28:27
+list method:-
+    1: append
+    2: clear
+    3: copy
+    4: count
+    5: extend
+    6: index
+    7: insert
+    8: pop
+    9: remove
+    10: reverse
+    11: sort
+print(dir(l2)) #to print functions and methods of list
+
+
+11:00 / 28:27
 """
 
 """defining list"""
 l1 = [] #empty list
-l2 = [3,2,4,"python",5,6] #list with values (any datatype)
+l2 = [3,2,4,"python","javaScript",5,6] #list with values (any datatype)
+
+print(l2) #to print list
+# print(l1, type(l1)) #to print list and its type
 
 """
 indexing
@@ -28,9 +46,19 @@ indexing by negative values
 """
 
 """accessing list values"""
-print(l1)
-print(l2)
-print(type(l2))
-print(l2[3]) #access python (by index)
-print(l2[-2]) #access 5 (by negative index)
+# print(l2[4]) #to find 4th index value
+# print(l2[-3]) #from last to 3rd value
+# print(l2[4][3]) #to find 'a' form javaScript
 
+
+"""slicing"""
+# print(l2) #no slicing
+# print(l2[:]) #from 0 to last
+# print(l2[0:]) #from 0 to last
+# print(l2[1:]) #from 1 to last
+# print(l2[1:4]) #from 1 to 3rd
+# print(l2[4][4:10]) #slice 'Script' from javaScript
+
+"""modifying values"""
+l2[3] = "Mojo" #to change python into mojo
+print(l2)
