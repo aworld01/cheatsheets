0619bb387240a5181be769ef2cdfad50d984e23c
Abdul Zoha <abdulzoha786@gmail.com>
2023-09-12 04:36:13 +0300

    On branch master Changes to be committed: 	new file:   python/AI/MagnetBrains/python/42_python_as_a_calculator.py 	new file:   python/tutorials/python@harshitVashisth/154_enumerate_function.py 	new file:   python/tutorials/python@me/done/42_python_as_a_calculator.py

---
+++ python/tutorials/python@me/done/42_python_as_a_calculator.py
@@ -0,0 +1,9 @@
+print(5+4) #Addition
+print(5-4) #Subtraction
+print(10*3) #Multiplication
+print(10/3) #Division
+print(10%3) #Modulus
+print(10//3) #Floor division
+print(2**5) #Exponentiation
+print(4**2) #Square
+print(4**3) #Cube
