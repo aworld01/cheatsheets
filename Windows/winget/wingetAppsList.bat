29aa35ab36425cb0b6aa2f94128a95f1df5cfa10
Windows 11 <abdulzoha786@gmail.com>
2025-01-27 13:40:51 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Windows/winget/wingetAppsList.bat
    	new file:   python/AI/nnfs/.ipynb_checkpoints/1_nnfs-checkpoint.ipynb
    	renamed:    python/AI/1_nnfs.ipynb -> python/AI/nnfs/1_nnfs.ipynb
    	new file:   python/AI/nnfs/pics/3 inputs 1 neuron.jpg
    	new file:   python/AI/nnfs/pics/4 inputs 1 neuron.jpg

--- Windows/winget/wingetAppsList.bat
+++ Windows/winget/wingetAppsList.bat
@@ -1,6 +1,5 @@
 winget source update
-winget install -e --id Google.Chrome --silent
+winget install -e --id Google.Chrome.EXE --silent
 winget install -e --id VideoLAN.VLC --silent
 winget install -e --id Microsoft.VisualStudioCode --silent
-winget install -e --id VideoLAN.VLC --silent
 winget install -e --id Audacity.Audacity --silent
